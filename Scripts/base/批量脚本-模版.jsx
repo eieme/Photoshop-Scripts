68127155314a88a61c9b918c89c66952c0649869
eieme <272493431@qq.com>
2018-08-31 13:57:23 +0800

    修改批量脚本的模板

--- Scripts/base/批量脚本-模版.jsx
+++ Scripts/base/批量脚本-模版.jsx
@@ -1,4 +1,5 @@
-﻿
+﻿#target photoshop
+
 res ="dialog { alignChildren:'fill',\
 text:'批量处理PSD ',\
         batGroup: Group{orientation: 'column',alignChildren:'left',\
@@ -31,28 +32,74 @@ function showDialog () {
    win.buttons.btnOK.onClick = function () {
         this.parent.parent.close();
         run();
+        //getAllFilesFromPath();
     }
     
       win.center();
       win.show();
  }
+
+function log(text){
+        $.writeln(text);
+        alert (text);
+}
+
+//获取文件夹下的文件
+function getAllFils(folder){
+    var filelist = folder.getFiles();
+    var result = [];
+    result = result.concat (filelist);
+    
+    for(var i = 0;i < filelist.length;i++){
+        var file = filelist[i];
+        if(file instanceof Folder){
+             result = result.concat (getAllFils(file));
+        }
+    }
+
+    return result ;
+}
+//获取路径下的所有文件
+function getAllFilesFromPath(path){
+      var openFolder = Folder(path);      
+      var filelist = getAllFils(openFolder);
+      $.writeln( filelist.length);
+      for(var i = 0;i < filelist.length;i++){
+            var file = filelist[i];
+            if(file.hidden == true){//不处理隐藏文件
+                continue;
+            }
+
+            if (file instanceof File){ 
+                $.writeln("File: "+file.fsName);
+                
+            }
+        
+            if(file instanceof Folder){
+                $.writeln("Folder: "+ file.fsName);
+            }
+            
+      }
+        return filelist;
+}
+
 function run(){
-   var openFolder = Folder(sourceFolder.string.text);                
-                var fileList = openFolder.getFiles() //获取open文件夹下所有文件
-                     if(fileList.length == 0){
-                         $.writeln("没有发现文档");
-                         alert ("没有发现文档");
-                        return;
-                     }
-                for (i=0;i<fileList.length;i++){
-                    if (fileList[i] instanceof File && fileList[i].hidden == false){ //不处理隐藏文件
-                        open(fileList[i]); 
-                       logic();
-                    }
-                }
-              $.writeln("完成并打开源文件夹");
-              alert ("完成并打开源文件夹");
-              new Folder(sourceFolder.string.text).execute();//打开文件夹
+    var flielist = getAllFilesFromPath(sourceFolder.string.text);
+    if(flielist.length == 0){
+        log("没有发现文档");
+        return;
+    }
+    for (i=0;i<flielist.length;i++){
+        var file = flielist[i];
+        if (file instanceof File){ //不处理隐藏文件
+            open(file); 
+            logic();
+        }
+    }
+
+      log ("完成并打开源文件夹");
+      
+      new Folder(sourceFolder.string.text).execute();//打开文件夹
  }
 
 function logic(){
@@ -60,9 +107,9 @@ function logic(){
     var name = decodeURI(app.activeDocument.name);
 	name = name.substring(0, name.indexOf("."));
     var dir = docPath + "/"+name+"/";
-	new Folder(dir).create();
+	new Folder(dir).create(); //创建文档同名目录
     
-	app.activeDocument.duplicate();
+	app.activeDocument.duplicate(); //创建文档副本
     
     //这里写 批处理的逻辑
  }
